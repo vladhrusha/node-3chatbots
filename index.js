8dd4e8d2b3b8476cec3e58589cc084b628abd6de
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-27 00:40:02 +0300

    attempt to launch at heroku

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN_THREEDOTONEABOUTMEBOT;
 let bot;
-// const bot = new TelegramBot(token, { polling: true });
-// bot.on("message", (msg) => {
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, "Hello World");
-// });
 
 if (process.env.NODE_ENV === "production") {
   bot = new TelegramBot(token);
