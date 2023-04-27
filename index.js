01c8c8ce88ef4d9953a1fed318dcf35698d6c193
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-27 16:27:03 +0300

    try fix

--- index.js
+++ index.js
@@ -77,6 +77,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from the Bot API." });
 });
+app.post("/", (req, res) => {
+  res.status(200).json({ message: "Hello from the Bot API." });
+});
 app.post(`/${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.status(200).json({ message: "ok" });
