17b5ea9e3f4bfc040c4714623c851b6e5eddc8db
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-27 15:16:56 +0300

    test 6

--- index.js
+++ index.js
@@ -1,59 +1,94 @@
-require("dotenv").config();
+// require("dotenv").config();
+
+// const TelegramBot = require("node-telegram-bot-api");
+// // const token = process.env.TOKEN_THREEDOTONEABOUTMEBOT;
+// const token = "6226322961:AAFUZl0yNlCXaeOjiwrIBgSOolk_OztvzSU";
+
+// // let bot;
+
+// // if (process.env.NODE_ENV === "production") {
+// //   bot = new TelegramBot(token);
+// //   // bot.setWebHook(process.env.HEROKU_URL + token);
+// //   bot.setWebHook(
+// //     `https://api.telegram.org/bot${token}/setWebhook?url=https://about-me-bot12.herokuapp.com/`,
+// //   );
+// //   // eslint-disable-next-line
+// //   console.log(process.env.NODE_ENV);
+// // } else {
+// //   bot = new TelegramBot(token, { polling: true });
+// // }
+
+// const bot = new TelegramBot(token);
+// // bot.setWebHook(process.env.HEROKU_URL + token);
+// bot.setWebHook(
+//   `https://api.telegram.org/bot${token}/setWebhook?url=https://about-me-bot12.herokuapp.com/`,
+// );
+
+// // eslint-disable-next-line
+
+// bot.on("message", async (msg) => {
+//   // eslint-disable-next-line
+//   console.log(msg);
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, "hello2");
+// });
+// //
+// // // eslint-disable-next-line
+// // bot.onText(/\about/, (msg) => {
+// //   const reply = "about";
+// //   const chatId = msg.chat.id;
+// //   bot.sendMessage(chatId, reply);
+// // });
+// // // eslint-disable-next-line
+// // bot.onText(/\links/, (msg) => {
+// //   const reply = "links";
+// //   const chatId = msg.chat.id;
+// //   bot.sendMessage(chatId, reply);
+// // });
+// // // eslint-disable-next-line
+// // bot.onText(/\start/, (msg) => {
+// //   const reply = "start";
+// //   const chatId = msg.chat.id;
+// //   bot.sendMessage(chatId, reply);
+// // });
+// // // eslint-disable-next-line
+// // bot.onText(/\help/, (msg) => {
+// //   const reply = "help2";
+// //   const chatId = msg.chat.id;
+// //   bot.sendMessage(chatId, reply);
+// // });
 
 const TelegramBot = require("node-telegram-bot-api");
-// const token = process.env.TOKEN_THREEDOTONEABOUTMEBOT;
+
+// replace the value below with the Telegram token you receive from @BotFather
+const port = process.env.PORT || 5000;
 const token = "6226322961:AAFUZl0yNlCXaeOjiwrIBgSOolk_OztvzSU";
 
-// let bot;
-
-// if (process.env.NODE_ENV === "production") {
-//   bot = new TelegramBot(token);
-//   // bot.setWebHook(process.env.HEROKU_URL + token);
-//   bot.setWebHook(
-//     `https://api.telegram.org/bot${token}/setWebhook?url=https://about-me-bot12.herokuapp.com/`,
-//   );
-//   // eslint-disable-next-line
-//   console.log(process.env.NODE_ENV);
-// } else {
-//   bot = new TelegramBot(token, { polling: true });
-// }
-
+// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token);
-// bot.setWebHook(process.env.HEROKU_URL + token);
-bot.setWebHook(
-  `https://api.telegram.org/bot${token}/setWebhook?url=https://about-me-bot12.herokuapp.com/`,
-);
+bot.startWebhook(`/${token}`, null, port);
+bot.telegram.setWebhook(`https://about-me-bot12.herokuapp.com/${token}`);
 
-// eslint-disable-next-line
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-bot.on("message", async (msg) => {
-  // eslint-disable-next-line
-  console.log(msg);
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "hello2");
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
-//
-// // eslint-disable-next-line
-// bot.onText(/\about/, (msg) => {
-//   const reply = "about";
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, reply);
-// });
-// // eslint-disable-next-line
-// bot.onText(/\links/, (msg) => {
-//   const reply = "links";
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, reply);
-// });
-// // eslint-disable-next-line
-// bot.onText(/\start/, (msg) => {
-//   const reply = "start";
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, reply);
-// });
-// // eslint-disable-next-line
-// bot.onText(/\help/, (msg) => {
-//   const reply = "help2";
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, reply);
-// });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "Received your message");
+});
+// eslint-disable-next-line
+console.log(`Server running on port ${port}`);
