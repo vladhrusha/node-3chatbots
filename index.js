879969ee1789e7541de0c3ab29759372855e6131
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-27 13:23:49 +0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const TelegramBot = require("node-telegram-bot-api");
-const token = process.env.TOKEN_THREEDOTONEABOUTMEBOT;
+// const token = process.env.TOKEN_THREEDOTONEABOUTMEBOT;
+const token = "6226322961:AAFUZl0yNlCXaeOjiwrIBgSOolk_OztvzSU";
 let bot;
 
 if (process.env.NODE_ENV === "production") {
@@ -16,27 +17,34 @@ if (process.env.NODE_ENV === "production") {
 // eslint-disable-next-line
 console.log("Bot server started in the " + process.env.NODE_ENV + " mode");
 
-// eslint-disable-next-line
-bot.onText(/\about/, (msg) => {
-  const reply = "about";
+bot.on("message", async (msg) => {
+  // eslint-disable-next-line
+  console.log(msg);
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, reply);
+  bot.sendMessage(chatId, "hello2");
 });
-// eslint-disable-next-line
-bot.onText(/\links/, (msg) => {
-  const reply = "links";
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, reply);
-});
-// eslint-disable-next-line
-bot.onText(/\start/, (msg) => {
-  const reply = "start";
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, reply);
-});
-// eslint-disable-next-line
-bot.onText(/\help/, (msg) => {
-  const reply = "help2";
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, reply);
-});
+
+// // eslint-disable-next-line
+// bot.onText(/\about/, (msg) => {
+//   const reply = "about";
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, reply);
+// });
+// // eslint-disable-next-line
+// bot.onText(/\links/, (msg) => {
+//   const reply = "links";
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, reply);
+// });
+// // eslint-disable-next-line
+// bot.onText(/\start/, (msg) => {
+//   const reply = "start";
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, reply);
+// });
+// // eslint-disable-next-line
+// bot.onText(/\help/, (msg) => {
+//   const reply = "help2";
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, reply);
+// });
