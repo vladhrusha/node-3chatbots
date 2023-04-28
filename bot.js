514cecc5906c9528f409a87378566ecc1a2a2c31
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-28 14:57:32 +0300

    separeted Bot code into another file

---
+++ bot.js
@@ -0,0 +1,48 @@
+const TelegramBot = require("node-telegram-bot-api");
+let bot;
+const token = "6226322961:AAFUZl0yNlCXaeOjiwrIBgSOolk_OztvzSU";
+
+if (process.env.NODE_ENV === "production") {
+  bot = new TelegramBot(token);
+  bot.setWebHook(`https://about-me-bot12.herokuapp.com/${token}`);
+} else {
+  bot = new TelegramBot(token, { polling: true });
+}
+
+// eslint-disable-next-line
+bot.onText(/\about/, async (msg) => {
+  const reply = "My name is Vlad";
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, reply);
+});
+// eslint-disable-next-line
+bot.onText(/\links/, async (msg) => {
+  const chatId = msg.chat.id;
+  const mySocials = {};
+  mySocials.linkedIn = "https://www.linkedin.com/in/vladyslavhrusha/";
+  mySocials.facebook = "https://www.facebook.com/vladyslav.hrusha";
+  const reply = `Welcome, here are my social links \n
+  - LinkenIn - ${mySocials.linkedIn}\n
+  - Facebook - ${mySocials.facebook}`;
+  bot.sendMessage(chatId, reply);
+});
+// eslint-disable-next-line
+bot.onText(/\help/, async (msg) => {
+  const reply = "Here is the list";
+  const chatId = msg.chat.id;
+
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ["/start - start bot"],
+        ["/help - list all commands"],
+        ["/links - provide my social links"],
+        ["/about - provide short info about me"],
+      ],
+    }),
+  };
+  bot.sendMessage(chatId, reply, opts);
+});
+
+module.exports = bot;
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const app = express();
 require("dotenv").config();
@@ -7,13 +6,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const token = "6226322961:AAFUZl0yNlCXaeOjiwrIBgSOolk_OztvzSU";
 
-let bot;
-if (process.env.NODE_ENV === "production") {
-  bot = new TelegramBot(token);
-  bot.setWebHook(`https://about-me-bot12.herokuapp.com/${token}`);
-} else {
-  bot = new TelegramBot(token, { polling: true });
-}
+const bot = require("./bot");
 
 app.use(express.json());
 
@@ -28,42 +21,6 @@ app.post(`/${token}`, (req, res) => {
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`\n\nServer running on port ${port}.\n\n`);
-});
-
-// eslint-disable-next-line
-bot.onText(/\about/, async (msg) => {
-  const reply = "My name is Vlad";
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, reply);
-});
-// eslint-disable-next-line
-bot.onText(/\links/, async (msg) => {
-  const chatId = msg.chat.id;
-  const mySocials = {};
-  mySocials.linkedIn = "https://www.linkedin.com/in/vladyslavhrusha/";
-  mySocials.facebook = "https://www.facebook.com/vladyslav.hrusha";
-  const reply = `Welcome, here are my social links \n
-  - LinkenIn - ${mySocials.linkedIn}\n
-  - Facebook - ${mySocials.facebook}`;
-  bot.sendMessage(chatId, reply);
-});
-// eslint-disable-next-line
-bot.onText(/\help/, async (msg) => {
-  const reply = "Here is the list";
-  const chatId = msg.chat.id;
-
-  const opts = {
-    reply_to_message_id: msg.message_id,
-    reply_markup: JSON.stringify({
-      keyboard: [
-        ["/start - start bot"],
-        ["/help - list all commands"],
-        ["/links - provide my social links"],
-        ["/about - provide short info about me"],
-      ],
-    }),
-  };
-  bot.sendMessage(chatId, reply, opts);
 });
 
 // // Listen for any kind of message. There are different kinds of
