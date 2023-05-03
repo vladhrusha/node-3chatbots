97d24e9943cf28fb5034471d643e05dd25b47d4d
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-05-03 21:09:36 +0300

    removed redundant code

--- holidayBot.js
+++ holidayBot.js
@@ -4,27 +4,12 @@ let bot;
 const token = process.env.HOLIDAYBOT_TOKEN;
 const url = process.env.HEROKU_URL;
 const logger = require("./utils/logger");
-const countryFlagEmoji = require("country-flag-emoji");
-const { flag, name, code } = require("country-emoji");
+const { name, code } = require("country-emoji");
 
 const fetchData = require("./holidayAPI");
 
 const countries = ["JP", "AL", "BE", "HR", "DK", "PE", "QA", "NO"];
 const countriesFlags = [];
-countries.forEach(async (country) => {
-  const countryData = await countryFlagEmoji.get(country);
-  const countryUnicode = countryData.unicode;
-  const unicodeSplit = countryUnicode.split(" ");
-  unicodeSplit.forEach((element, ind) => {
-    const index = element.indexOf("+");
-    unicodeSplit[ind] = element.substring(index + 1);
-  });
-  const codePoints = unicodeSplit.map((element) => parseInt(element, 16));
-  // eslint-disable-next-line
-  const countryFlag =
-    String.fromCodePoint(...codePoints) + " " + countryData.name;
-  countriesFlags.push(flag(countryData.name));
-});
 
 const establishConnection = () => {
   try {
@@ -41,19 +26,6 @@ const establishConnection = () => {
 };
 
 establishConnection();
-// eslint-disable-next-line
-function toUnicodeString(emojiString) {
-  let unicodeString = "";
-  for (let i = 0; i < emojiString.length; i++) {
-    const codePoint = emojiString.codePointAt(i);
-    const hexString = codePoint.toString(16).toUpperCase();
-    unicodeString += `U+${hexString} `;
-    if (codePoint > 0xffff) {
-      i++;
-    }
-  }
-  return unicodeString.trim();
-}
 
 bot.on("message", async (msg) => {
   try {
@@ -65,18 +37,6 @@ bot.on("message", async (msg) => {
       if (data.length > 0) {
         const reply = data[0].name;
         await bot.sendMessage(chatId, name(input) + " - " + reply);
-        const countryData = await countryFlagEmoji.get(input);
-        const countryUnicode = countryData.unicode;
-        const unicodeSplit = countryUnicode.split(" ");
-        unicodeSplit.forEach((element, ind) => {
-          const index = element.indexOf("+");
-          unicodeSplit[ind] = element.substring(index + 1);
-        });
-        const codePoints = unicodeSplit.map((element) => parseInt(element, 16));
-        // eslint-disable-next-line
-        const countryFlag = String.fromCodePoint(...codePoints);
-
-        // await bot.sendMessage(chatId, countryFlag);
       } else
         await bot.sendMessage(chatId, name(input) + " - " + "not a holiday");
     }
